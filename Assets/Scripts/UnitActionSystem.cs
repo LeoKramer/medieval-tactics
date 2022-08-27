fdadadbcd812b968cd046ac76a893c7c9894c9b0
Leonardo Kramer <kramer.leo.94@gmail.com>
2022-08-27 16:59:22 -0400

    [NEW] level grid system

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -16,7 +16,7 @@ public class UnitActionSystem : MonoBehaviour
     {
         if (Instance != null)
         {
-            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+            Debug.LogError($"There's more than one UnitActionSystem! {transform} - {Instance}");
             Destroy(gameObject);
             return;
         }
